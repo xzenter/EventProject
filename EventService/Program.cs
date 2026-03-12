656e5efc3d27c4918a62f8b9cbf72cce82b45932
Anton Mayorov <xzenter@yandex.ru>
2026-03-12 20:45:45 +0300

    Настройка di

--- EventService/Program.cs
+++ EventService/Program.cs
@@ -1,6 +1,9 @@
+using EventService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IEventService, EventService.Services.EventService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
