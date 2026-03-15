0c112887eb9be3a259c4321e241b7ab8a348cf8a
Anton Mayorov <xzenter@yandex.ru>
2026-03-15 17:12:53 +0300

    Добавляет Swagger

--- EventProject/Program.cs
+++ EventProject/Program.cs
@@ -1,16 +1,23 @@
+using System.Reflection;
 using EventProject.Mapping;
 using EventProject.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddAutoMapper(mapper=>mapper.AddProfile<EventMappingProfile>());
-
+builder.Services.AddAutoMapper(mapper => mapper.AddProfile<EventMappingProfile>());
 builder.Services.AddScoped<IEventService, EventService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSwaggerGen(options =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
@@ -18,6 +25,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
