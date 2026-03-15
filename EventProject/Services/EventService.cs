db5098e0c0fbdacd79abecf4210c5382bc7d4111
Anton Mayorov <xzenter@yandex.ru>
2026-03-15 16:51:31 +0300

    Добавление реализации EventService

--- EventProject/Services/EventService.cs
+++ EventProject/Services/EventService.cs
@@ -1,6 +1,77 @@
+using AutoMapper;
+using EventProject.Controllers.Events.Dto;
+using EventProject.Models;
+
 namespace EventProject.Services;
 
 public class EventService : IEventService
 {
-    
+    private readonly IMapper _mapper;
+    private static readonly List<Event> Events = [];
+
+    public EventService(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public IEnumerable<EventDto> GetEvents()
+    {
+        return _mapper.Map<IEnumerable<EventDto>>(Events);
+    }
+
+    public EventDto? GetEvent(Guid id)
+    {
+        var @event = Events.FirstOrDefault(e => e.Id == id);
+
+        return @event == null
+            ? null
+            : _mapper.Map<EventDto>(@event);
+    }
+
+    public EventDto CreateEvent(EventForCreationDto eventDto)
+    {
+        var @event = _mapper.Map<Event>(eventDto);
+
+        @event.Id = Guid.NewGuid();
+
+        Events.Add(@event);
+
+        return _mapper.Map<EventDto>(@event);
+    }
+
+    public EventDto UpdateEvent(Guid id, EventForUpdateDto eventForUpdateDto)
+    {
+        var @event = Events.FirstOrDefault(e => e.Id == id);
+
+        if (@event == null)
+        {
+            throw new Exception($"Событие с id = {id} не найдено");
+        }
+
+        @event = new Event
+        {
+            Id = id,
+            Title = eventForUpdateDto.Title,
+            Description = eventForUpdateDto.Description,
+            StartAt = eventForUpdateDto.StartAt,
+            EndAt = eventForUpdateDto.EndAt
+        };
+
+        return _mapper.Map<EventDto>(@event);
+    }
+
+    public void DeleteEvent(Guid id)
+    {
+        var @event = Events.FirstOrDefault(e => e.Id == id);
+
+        if (@event == null)
+        {
+            return;
+        }
+
+        if (!Events.Remove(@event))
+        {
+            throw new Exception($"Событие с id = {id} не удалось удалить");
+        }
+    }
 }
--- EventProject/Services/IEventService.cs
+++ EventProject/Services/IEventService.cs
@@ -1,6 +1,12 @@
+using EventProject.Controllers.Events.Dto;
+
 namespace EventProject.Services;
 
 public interface IEventService
 {
-    
+    IEnumerable<EventDto> GetEvents();
+    EventDto? GetEvent(Guid id);
+    EventDto CreateEvent(EventForCreationDto eventDto);
+    EventDto UpdateEvent(Guid id, EventForUpdateDto eventForUpdateDto);
+    void DeleteEvent(Guid id);
 }
