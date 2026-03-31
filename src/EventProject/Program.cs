c5e4313847d1cc13ec095ad635ea245852675085
Anton Mayorov <xzenter@yandex.ru>
2026-03-31 11:13:11 +0300

    Исправление загрузки репозитория в DI

--- src/EventProject/Program.cs
+++ src/EventProject/Program.cs
@@ -6,7 +6,7 @@ using EventProject.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddScoped<IEventRepository, EventRepository>();
+builder.Services.AddSingleton<IEventRepository, EventRepository>();
 builder.Services.AddScoped<IEventService, EventService>();
 
 builder.Services.AddControllers();
