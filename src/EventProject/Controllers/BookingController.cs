213fd736de387d55502822f90765bd3498f08ed7
Anton Mayorov <xzenter@yandex.ru>
2026-04-12 11:14:07 +0300

    Разделение контроллеров

---
+++ src/EventProject/Controllers/BookingController.cs
@@ -0,0 +1,42 @@
+using EventProject.Models;
+using EventProject.Services.Booking;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventProject.Controllers;
+
+[ApiController]
+[Produces("application/json")]
+public class BookingController(
+    IBookingService bookingService
+) : ControllerBase
+{
+    /// <summary>
+    /// Создать бронирование для указанного события.
+    /// </summary>
+    /// <param name="id">Идентификатор события</param>
+    /// <response code="202">Бронирование успешно создано.</response>
+    /// <response code="404">Событие для бронирования не найдено.</response>
+    [ProducesResponseType(StatusCodes.Status202Accepted)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpPost("/events/{id:guid}/book")]
+    public async Task<IActionResult> CreateBooking(Guid id)
+    {
+        var bookingDto = await bookingService.CreateBookingAsync(id);
+        return Accepted($"/bookings/{bookingDto.BookingId}", bookingDto);
+    }
+
+    /// <summary>
+    /// Получение брони по идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор бронирования.</param>
+    /// <response code="200">Бронирование получено.</response>
+    /// <response code="404">Бронирование не найдено.</response>
+    [ProducesResponseType(typeof(Booking), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [HttpGet("/bookings/{id:guid}")]
+    public async Task<ActionResult<Booking>> GetBooking(Guid id)
+    {
+        var bookingDto = await bookingService.GetBookingByIdAsync(id);
+        return Ok(bookingDto);
+    }
+}
--- src/EventProject/Controllers/EventsController.cs
+++ src/EventProject/Controllers/EventsController.cs
@@ -1,7 +1,5 @@
 using EventProject.Dto.Query;
 using EventProject.Dto.Response;
-using EventProject.Models;
-using EventProject.Services.Booking;
 using EventProject.Services.Event;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,8 +9,7 @@ namespace EventProject.Controllers;
 [Route("events")]
 [Produces("application/json")]
 public class EventsController(
-    IEventService eventService,
-    IBookingService bookingService
+    IEventService eventService
 ) : ControllerBase
 {
     /// <summary>
@@ -85,35 +82,5 @@ public class EventsController(
     {
         eventService.DeleteEvent(id);
         return Ok();
-    }
-
-    /// <summary>
-    /// Создать бронирование для указанного события.
-    /// </summary>
-    /// <param name="id">Идентификатор события</param>
-    /// <response code="202">Бронирование успешно создано.</response>
-    /// <response code="404">Событие для бронирования не найдено.</response>
-    [ProducesResponseType(StatusCodes.Status202Accepted)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [HttpPost("{id:guid}/book")]
-    public async Task<IActionResult> CreateBooking(Guid id)
-    {
-        var bookingDto = await bookingService.CreateBookingAsync(id);
-        return AcceptedAtRoute($"/bookings/{bookingDto.BookingId}", bookingDto);
-    }
-
-    /// <summary>
-    /// Получение брони по идентификатору.
-    /// </summary>
-    /// <param name="id">Идентификатор бронирования.</param>
-    /// <response code="200">Бронирование получено.</response>
-    /// <response code="404">Бронирование не найдено.</response>
-    [ProducesResponseType(typeof(Booking), StatusCodes.Status200OK)]
-    [ProducesResponseType(StatusCodes.Status404NotFound)]
-    [HttpGet("/bookings/{id:guid}")]
-    public async Task<ActionResult<Booking>> GetBooking(Guid id)
-    {
-        var bookingDto = await bookingService.GetBookingByIdAsync(id);
-        return Ok(bookingDto);
     }
 }
