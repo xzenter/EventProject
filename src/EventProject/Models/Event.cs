2a1202737eafa7ab00feb6a33b8dcc9ca609ebc7
Anton Mayorov <xzenter@yandex.ru>
2026-04-28 15:01:49 +0300

    Обновление Event

--- src/EventProject/Models/Event.cs
+++ src/EventProject/Models/Event.cs
@@ -7,4 +7,30 @@ public class Event
     public string? Description { get; set; }
     public required DateTime StartAt { get; set; }
     public required DateTime EndAt { get; set; }
+
+    public required int TotalSeats { get; set; }
+
+    public int AvailableSeats { get; set; }
+
+    public bool TryReserveSeats(int count = 1)
+    {
+        if (AvailableSeats >= count)
+        {
+            AvailableSeats -= count;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public void ReleaseSeats(int count = 1)
+    {
+        AvailableSeats += count;
+
+        if (AvailableSeats > TotalSeats)
+        {
+            AvailableSeats = TotalSeats;
+        }
+    }
 }
