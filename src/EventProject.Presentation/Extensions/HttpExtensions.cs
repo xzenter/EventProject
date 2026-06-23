4f43c03b396295cdbd7ae9cd7b2eb8b0a9ee5054
Anton Mayorov <xzenter@yandex.ru>
2026-06-23 11:18:15 +0300

    Рефакторинг

--- src/EventProject.Presentation/Extensions/HttpExtensions.cs
+++ src/EventProject.Presentation/Extensions/HttpExtensions.cs
@@ -4,16 +4,11 @@ namespace EventProject.Presentation.Extensions;
 
 public static class HttpExtensions
 {
-    public static bool TryGetUser(this HttpContext ctx, out User record)
-    {
-        var result = ctx.Items.TryGetValue(nameof(User), out var property);
-        record = property as User;
-        return result;
-    }
-
     public static User GetUser(this HttpContext ctx)
     {
-        if (ctx.TryGetUser(out var user)) return user;
+        if (ctx.Items.TryGetValue(nameof(User), out var value) && value is User user)
+            return user;
+
         throw new InvalidOperationException("Пользователь не найден в контексте HTTP");
     }
 }
